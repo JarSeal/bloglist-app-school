5a789fb1910fa0ec17698ad98558635d2e62aa9a
JarSeal <kai.forsman@gmail.com>
2021-01-03 10:58:25 +0800

    New healthcheck.

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,10 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
-const healthRouter = require('express').Router();
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const healthRouter = require('./controllers/health');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -39,15 +39,12 @@ if(process.env.SERVE_STATIC === 'production') {
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/health', healthRouter);
 
 if(process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing');
   app.use('/api/testing', testingRouter);
 }
-
-healthRouter.get('/health', async (request, response) => {
-  response.send('ok');
-});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
